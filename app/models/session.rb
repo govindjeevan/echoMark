7b159f448c7244de2e5dab1e7703e843a37c34ac
Govind Jeevan <govindjeevan7@gmail.com>
2020-01-26 10:01:56 +0530

    random string

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,8 +3,7 @@ class Session < ApplicationRecord
 
 
   def generate_key
-    require 'securerandom'
-    random_string = self.id
+    random_string = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
     random_string
   end
 end
