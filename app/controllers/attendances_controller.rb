9934283588d085ddb49144487994921c8292e272
Govind Jeevan <govindjeevan7@gmail.com>
2020-01-26 11:43:48 +0530

    session added

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -77,6 +77,6 @@ class AttendancesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def attendance_params
-    params.permit(:user_id, :key)
+    params.permit(:user_id, :key, :session_id)
   end
 end
