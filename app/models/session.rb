e41f4408303afe45126da9a181f4d7dfef4ab7ab
Govind Jeevan <govindjeevan7@gmail.com>
2020-01-26 09:42:52 +0530

    caesar cipher

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,10 +1,23 @@
 class Session < ApplicationRecord
   belongs_to :user
 
+  def caesar_cipher (string, number)
+    caesar_string = ""
+
+    string.scan (/./) do |i|
+      if ("a".."z").include? (i.downcase) # Identify letters only.
+        number.times {i = i.next}
+      end
+      caesar_string << i[-1]
+    end
+    return caesar_string
+
+  end
+
 
   def generate_key
     require 'securerandom'
-    random_string = SecureRandom.hex
+    random_string = caesar_cipher(self.created_at,5)
     random_string
   end
 end
